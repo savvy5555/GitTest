8f79fa271809bfdf7b27a01007c12c7628fe59bc
Savvy <savvy5555@gmai.com>
2025-06-30 19:49:01 -0500

    Initial commit

---
+++ src/main/java/com/itexpsj/gittest/GitTest.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.itexpsj.gittest;
+
+/**
+ *
+ * @author savvy
+ */
+public class GitTest {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
