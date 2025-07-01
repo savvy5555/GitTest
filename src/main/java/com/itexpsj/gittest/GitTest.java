b98abe395277985c79c3db164cbece6a455b421e
Savvy <savvy5555@gmai.com>
2025-06-30 19:57:25 -0500

    Some More Changes

--- src/main/java/com/itexpsj/gittest/GitTest.java
+++ src/main/java/com/itexpsj/gittest/GitTest.java
@@ -13,5 +13,6 @@ public class GitTest {
     public static void main(String[] args) {
         System.out.println("Hello World!");
         System.out.println("Happy Monday");
+        System.out.println("Learning Git Commands");
     }
 }
